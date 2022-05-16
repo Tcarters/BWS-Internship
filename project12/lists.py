8b68347478841dcfa095630f49f06663b40eb206
Tcarters <cartersedmund@gmail.com>
2022-05-16 23:50:43 +0530

    update1..

---
+++ project12/lists.py
@@ -0,0 +1,9 @@
+# List is an array in js and php
+
+
+lst = [ "String", 1, 3.14, ["A new item"], "Kalob" ]
+
+for item in lst:
+    print("The item is:",  item )
+
+print ( "Data from list : ", lst )
