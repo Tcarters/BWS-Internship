1ed7f839b1ead7876462d540a437f1145e4b2a33
Tcarters <cartersedmund@gmail.com>
2022-05-17 03:38:31 +0530

    update2..

---
+++ project12/dictionaries.py
@@ -0,0 +1,16 @@
+person = {
+    "key": "value",
+    "name": "Tcarters",
+    "twitter": "@TcartersKaty"
+
+}
+
+print(person['twitter'])
+
+print("Bfore:", person, "\n")
+
+person['instagram'] = "@coding.for.everyone"
+
+# del person['key']
+
+print(person)
---
+++ project12/sets.py
@@ -0,0 +1,10 @@
+# lst = [varname, varname2, varname3 ]
+
+# dictionary = {
+#     "key": "value",
+
+# }
+
+# sets change the order of item placed in the sets
+s = {"Item 1", "Item 2", "Item 3" }
+print (s)
