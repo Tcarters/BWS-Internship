76dc65d25b2fda3220cd377bb1c7974e66491598
Tcarters <cartersedmund@gmail.com>
2022-05-26 21:11:15 +0530

    new files...

---
+++ project14/class_cleanup.py
@@ -0,0 +1,16 @@
+
+class Animal:
+    fur_color = "Orange"
+
+    def speak(self):
+        print("Rawat ")
+
+    def eat(self):
+        pass
+
+    def chase(self):
+        pass
+
+calling = Animal()
+
+print ( calling.speak() )
---
+++ project14/first_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class myclass:
+    this_is_a_property = "Something"
+
+    #pass
+the_animal = myclass()
+
+print (the_animal.this_is_a_property)
