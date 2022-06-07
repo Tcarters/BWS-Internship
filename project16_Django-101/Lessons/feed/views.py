066fd68433cd227d7aa826c24cf68b68fd1d16a1
Tcarters <cartersedmund@gmail.com>
2022-06-07 23:54:31 +0530

    u1

--- project16_Django-101/Lessons/feed/views.py
+++ project16_Django-101/Lessons/feed/views.py
@@ -5,7 +5,13 @@
 #Use class based views
 
 from django.views.generic import TemplateView
+# from matplotlib.style import context
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_thing'] = "Hello world :P this is "
+        return context
