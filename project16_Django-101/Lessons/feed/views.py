c3a99ec46c1991eef3131808a1ac29002f3c89b0
Tcarters <cartersedmund@gmail.com>
2022-06-15 23:24:11 +0530

    create a post

--- project16_Django-101/Lessons/feed/views.py
+++ project16_Django-101/Lessons/feed/views.py
@@ -7,11 +7,13 @@
 from django.views.generic import TemplateView
 # from matplotlib.style import context
 
+from .models import Post
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['my_thing'] = "Hello world :P this is dynamic"
+        # context['my_thing'] = "Hello world :P this is dynamic"
+        context['posts'] = Post.objects.all()
         return context
