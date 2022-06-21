285f563e0928c45e13859009376f172915948df6
Tcarters <cartersedmund@gmail.com>
2022-06-21 23:42:16 +0530

    date field in model

---
+++ project17_Django-201/cmd.sh
@@ -0,0 +1,13 @@
+pip install Django
+
+pip show Django
+
+django-admin startproject today i learn or (til)
+
+pipenv shell
+
+python3 manage.py createsuperuser
+
+python3 manage.py runserver 1234
+
+python3 manage.py startapp feed  #===> Start a new App
