ba36f8da4514d58f49d91be2f0938973c9628a0d
Tcarters <cartersedmund@gmail.com>
2022-06-23 00:51:22 +0530

    allauth package added & implemented

--- project17_Django-201/Lessons/pro/urls.py
+++ project17_Django-201/Lessons/pro/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django import urls
 from django.conf.urls  import include
 from django.contrib import admin
 from django.urls import path
@@ -21,5 +22,6 @@ from feed import urls as feed_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(feed_urls, namespace="feed" ) )
+    path("", include(feed_urls, namespace="feed" ) ),
+    path("", include("allauth.urls")),
 ]
