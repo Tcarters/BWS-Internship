53d82ada05de3c1fff9d1c8bea02b7ac7d74f8a3
Tcarters <cartersedmund@gmail.com>
2022-06-24 23:43:07 +0530

    adding styledpost

--- project17_Django-201/cmd.sh
+++ project17_Django-201/cmd.sh
@@ -11,3 +11,5 @@ python3 manage.py createsuperuser
 python3 manage.py runserver 1234
 
 python3 manage.py startapp feed  #===> Start a new App
+
+pip install django-allauth OR pipenv install django-allauth # To install django-allauth package.
