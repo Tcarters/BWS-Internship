2f7aef18ebd51057fbde93976a386efd1d420a97
Tcarters <cartersedmund@gmail.com>
2022-06-25 20:21:34 +0530

    ..till tailwindCSS model added

--- project17_Django-201/Lessons/feed/views.py
+++ project17_Django-201/Lessons/feed/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
@@ -19,4 +21,21 @@ class PostDetailView(DetailView):
     template_name = "feed/detail.html"
     model = Post
     context_object_name = "post"
-    
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "feed/create.html"
+    fields = ['text']
+
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
